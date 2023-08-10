b34af178976f19b649d2e049c8489360ed2efdfd
Knut Melvær <knut.melvaer@gmail.com>
2023-08-10 14:24:46 +0200

    Add Sanity/Astro integration (#14)
    * Install Astro+Sanity integration
    
    * add sanity integration config
    
    * replace vanilla client with sanity integration
    
    * copy fixes
    
    * upgrade astro integration

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,4 +1,22 @@
+// Loading environment variables from .env files
+// https://docs.astro.build/en/guides/configuring-astro/#environment-variables
+import { loadEnv } from "vite";
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+if (
+  !PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET
+) {
+  throw new Error("Did you forget to run sanity init --env?");
+}
+
 import { defineConfig } from "astro/config";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: PUBLIC_SANITY_PROJECT_ID,
+    dataset: PUBLIC_SANITY_DATASET,
+    useCdn: false, // `false` if you want to ensure fresh data
+    apiVersion: "2023-03-20", // date of setup
+  })]
+});
--- app/src/utils/sanity.ts
+++ app/src/utils/sanity.ts
@@ -1,30 +1,16 @@
-import { createClient } from "@sanity/client";
+import { useSanityClient } from "@sanity/astro";
 import type { PortableTextBlock } from "@portabletext/types";
 import type { ImageAsset, Slug } from "@sanity/types";
 import groq from "groq";
 
-if (
-  !import.meta.env.PUBLIC_SANITY_PROJECT_ID ||
-  !import.meta.env.PUBLIC_SANITY_DATASET
-) {
-  throw new Error("Did you forget to run sanity init --env?");
-}
-
-export const client = createClient({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: false, // `false` if you want to ensure fresh data
-  apiVersion: "2023-03-20", // date of setup
-});
-
 export async function getPosts(): Promise<Post[]> {
-  return await client.fetch(
+  return await useSanityClient().fetch(
     groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`
   );
 }
 
 export async function getPost(slug: string): Promise<Post> {
-  return await client.fetch(
+  return await useSanityClient().fetch(
     groq`*[_type == "post" && slug.current == $slug][0]`,
     {
       slug,
