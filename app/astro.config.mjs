19de2101ef38beee7b5264296c05fe93ca35ea91
Knut Melvær <knut@sanity.io>
2023-09-02 09:24:28 -0700

    Add embedded Studio

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,22 +1,34 @@
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
 import { loadEnv } from "vite";
-const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(import.meta.env.MODE, process.cwd(), "");
-if (
-  !PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET
-) {
-  throw new Error("Did you forget to run sanity init --env?");
-}
+
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  ""
+);
 
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+// Change this depending on your hosting provider (Vercel, Netlify etc)
+// https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
+import nodejs from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid", // Hybrid+adapter is required to support embedded Sanity Studio
+  adapter: nodejs({
+    mode: "middleware",
+  }),
   integrations: [sanity({
-    projectId: PUBLIC_SANITY_PROJECT_ID,
-    dataset: PUBLIC_SANITY_DATASET,
+    projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
+    dataset: PUBLIC_SANITY_STUDIO_DATASET,
+    studioBasePath: "/admin",
     useCdn: false, // `false` if you want to ensure fresh data
-    apiVersion: "2023-03-20", // date of setup
-  })]
+    apiVersion: "2023-03-20" // Set to date of setup to use the latest API version
+  }),
+  react() // Required for Sanity Studio
+  ]
 });
---
+++ app/sanity.config.ts
@@ -0,0 +1,29 @@
+const projectId = import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID!;
+const dataset = import.meta.env.PUBLIC_SANITY_STUDIO_DATASET!;
+
+// Feel free to remove this check if you don't need it
+if (!projectId || !dataset) {
+  throw new Error(
+    `Missing environment variable(s). Check if named correctly in .env file.\n\nShould be:\nPUBLIC_SANITY_STUDIO_PROJECT_ID=${projectId}\nPUBLIC_SANITY_STUDIO_DATASET=${dataset}\n\nAvailable environment variables:\n${JSON.stringify(
+      import.meta.env,
+      null,
+      2
+    )}`
+  );
+}
+
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schema";
+
+export default defineConfig({
+  name: "project-name",
+  title: "Project Name",
+  projectId,
+  dataset,
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+});
