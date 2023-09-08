5ccfd6a25b71bccda5ef530299e673291cbf2c9a
Knut Melvær <knut.melvaer@gmail.com>
2023-09-08 12:53:11 -0700

    fix: add vercel native env variables as fallback (#25)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@
 import { loadEnv } from "vite";
 const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
-  PUBLIC_SANITY_STUDIO_DATASET
+  PUBLIC_SANITY_STUDIO_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 import { defineConfig } from "astro/config";
+
+// Different environments use different variables
+const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
@@ -19,8 +26,8 @@ export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
   integrations: [sanity({
-    projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
-    dataset: PUBLIC_SANITY_STUDIO_DATASET,
+    projectId,
+    dataset,
     studioBasePath: "/admin",
     useCdn: false,
     // `false` if you want to ensure fresh data
