3ea52c1fb8eb7d8eacd67776de1bb084acb38ac5
Dave Li <dave@holee.tech>
2024-06-28 14:40:00 +0200

    Add support for Visual Editing.

---
+++ src/utils/load-query.ts
@@ -0,0 +1,41 @@
+import { type QueryParams } from "sanity";
+import { sanityClient } from "sanity:client";
+
+const visualEditingEnabled =
+  import.meta.env.SANITY_VISUAL_EDITING_ENABLED === "true";
+const token = import.meta.env.SANITY_API_READ_TOKEN;
+
+export async function loadQuery<QueryResponse>({
+  query,
+  params,
+}: {
+  query: string;
+  params?: QueryParams;
+}) {
+  if (visualEditingEnabled && !token) {
+    throw new Error(
+      "The `SANITY_API_READ_TOKEN` environment variable is required during Visual Editing."
+    );
+  }
+
+  const perspective = visualEditingEnabled ? "previewDrafts" : "published";
+
+  const { result, resultSourceMap } = await sanityClient.fetch<QueryResponse>(
+    query,
+    params ?? {},
+    {
+      filterResponse: false,
+      perspective,
+      resultSourceMap: visualEditingEnabled ? "withKeyArraySelector" : false,
+      stega: visualEditingEnabled,
+      ...(visualEditingEnabled ? { token } : {}),
+      useCdn: !visualEditingEnabled,
+    }
+  );
+
+  return {
+    data: result,
+    sourceMap: resultSourceMap,
+    perspective,
+  };
+}
