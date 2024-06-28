6b4e152fb85ac8b4c2b698687479bb0776f9e127
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-06-28 09:14:36 +0200

    chore(deps): fully upgrade @sanity/astro and friends

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,8 +1,8 @@
 import imageUrlBuilder from "@sanity/image-url";
 import type { Image } from "@sanity/types";
-import { useSanityClient } from "@sanity/astro";
+import { sanityClient } from "sanity:client";
 
-const builder = imageUrlBuilder(useSanityClient());
+const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(source: Image) {
   return builder.image(source);
--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,16 +1,16 @@
-import { useSanityClient } from "@sanity/astro";
+import { sanityClient } from "sanity:client";
 import type { PortableTextBlock } from "@portabletext/types";
 import type { ImageAsset, Slug } from "@sanity/types";
 import groq from "groq";
 
 export async function getPosts(): Promise<Post[]> {
-  return await useSanityClient().fetch(
+  return await sanityClient.fetch(
     groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`
   );
 }
 
 export async function getPost(slug: string): Promise<Post> {
-  return await useSanityClient().fetch(
+  return await sanityClient.fetch(
     groq`*[_type == "post" && slug.current == $slug][0]`,
     {
       slug,
