c210cc8aa4406fef2dca4a62d790142ec914c9c2
KenJonesPizza <kenjwho@gmail.com>
2026-04-13 18:02:26 -0500

    Add @sanity/code-input plugin with syntax-highlighted frontend rendering
    - Install and configure @sanity/code-input in the Studio
    - Add code type to blockContent schema for use in post body
    - Create CodeBlock.astro component using Shiki (bundled with Astro) for
      syntax highlighting with the github-dark-dimmed theme
    - Wire up CodeBlock as a custom PortableText component for code types
    - Map unsupported Shiki languages (groq, astro) to compatible aliases
    - Fix font variable typo: IMB Plex Mono -> IBM Plex Mono

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {codeInput} from '@sanity/code-input'
 import {defineDocuments, defineLocations, presentationTool} from 'sanity/presentation'
 import {schemaTypes} from './src/schemaTypes'
 
@@ -44,6 +45,7 @@ export default defineConfig({
       },
     }),
     visionTool(),
+    codeInput(),
   ],
   schema: {types: schemaTypes},
 })
--- studio/src/schemaTypes/objects/blockContent.tsx
+++ studio/src/schemaTypes/objects/blockContent.tsx
@@ -56,5 +56,8 @@ export default defineType({
         ],
       },
     }),
+    defineArrayMember({
+      type: 'code',
+    }),
   ],
 })
